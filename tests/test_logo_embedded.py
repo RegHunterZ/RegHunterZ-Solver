406415827adbc603f4164b56ba668345248e7261
RegHunterZ <kkpokerhun@gmail.com>
2025-10-18 01:35:29 +0200

    Delete tests directory

--- tests/test_logo_embedded.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-from PyQt6.QtWidgets import QApplication
-app = QApplication.instance() or QApplication(sys.argv)
-from ai_coach.ui.main_window import MainWindow
-
-def test_embedded_logo_loads():
-    w = MainWindow()
-    assert not w._load_logo_pix().isNull()
