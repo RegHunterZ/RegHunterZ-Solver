6274a7104df11f472c12933e734577e4d543a031
RegHunterZ <kkpokerhun@gmail.com>
2025-10-18 05:01:57 +0200

    Set up server to listen on specified port

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on ${port}`));
+
 const express = require('express');
 const path = require('path');
 const app = express();
